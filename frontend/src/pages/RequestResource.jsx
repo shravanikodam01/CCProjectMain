73af64c38bde45434387dba06a8bd3ce091c3e17
Shravani Kodam <shravanikodam01>
2023-12-07 18:36:18 -0600

    commiting changes

--- frontend/src/pages/RequestResource.jsx
+++ frontend/src/pages/RequestResource.jsx
@@ -4,23 +4,50 @@ import axios from "axios";
 
 const RequestResource = () =>{
     const {username, setUsername, availableResources, setAvailableResources} = useContext(UserContext)
-
+    
     useEffect(()=>{
         const fetchAllResources = async ()=>{
+            let resourcesAV=[], occupiedResourcesName=[]
             try{
-                const res = await axios.get('http://localhost:8800/resources-count')
-                const res2 = await axios.get('http://localhost:8800/occupied-resources-count')
-                console.log(res.data)
-                console.log(res2.data)
-                setAvailableResources(res.data)
+                const resourcesCount = await axios.get('http://localhost:8800/resources-count')
+                const occupiedResourcesCount = await axios.get('http://localhost:8800/occupied-resources-count')
+                
+                for(let i=0;i< resourcesCount.data.length;i++){
+                    for(let j=0;j<occupiedResourcesCount.data.length;j++){
+                        if(resourcesCount.data[i].type_name == occupiedResourcesCount.data[j].type_name){
+                            resourcesAV = [...resourcesAV, {type_name: resourcesCount.data[i].type_name, count: resourcesCount.data[i].count - occupiedResourcesCount.data[j].count}]
+                        }
+                    }
+                }
+                for(let i=0;i<occupiedResourcesCount.data.length;i++){
+                    occupiedResourcesName.push(occupiedResourcesCount.data[i].type_name)
+                }
+                for(let i=0;i<resourcesCount.data.length;i++){
+                    if(!occupiedResourcesName.includes(resourcesCount.data[i].type_name)){
+                        resourcesAV = [...resourcesAV, {type_name: resourcesCount.data[i].type_name, count: resourcesCount.data[i].count}]
+                    }
+                }
+                setAvailableResources(resourcesAV)
             }catch(err){
                 console.log(err)
             }
         }
         fetchAllResources()
     },[])
+    console.log(availableResources)
     return(
-        <div></div>
+        <>
+            <div className="resources-available-div">
+                {availableResources.map(resource=>{
+                    return(
+                        (resource.count > 0) && <div className="available-resource">
+                            <h4>{resource.type_name}</h4>
+                            <button>Add to Cart</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
 
